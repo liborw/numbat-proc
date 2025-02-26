62a39b77dbbc2a8a089f69f0047af07bb81f8c78
Libor Wagner <libor.wagner@t14s>
2025-02-26 15:28:45 +0100

    Evaluate "let a = 1 + 1 #=" correctly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 use std::{fs::File, io::{self, BufRead, Cursor}, path::PathBuf};
 use anyhow::{bail, Context as AnyhowContext, Result};
-use numbat::{module_importer::{BuiltinModuleImporter, ChainedImporter, FileSystemImporter}, resolver::CodeSource, Context};
+use numbat::{compact_str::CompactString, module_importer::{BuiltinModuleImporter, ChainedImporter, FileSystemImporter}, resolver::CodeSource, Context};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -21,21 +21,6 @@ struct Args {
 }
 
 
-struct Config {
-    equals_prompt: String,
-    load_prelude: bool,
-    load_user_init: bool,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            equals_prompt: "#=".to_string(),
-            load_prelude: true,
-            load_user_init: true,
-        }
-    }
-}
 
 #[derive(Debug, Clone)]
 enum Input {
@@ -62,12 +47,72 @@ impl Input {
 }
 
 struct Cli {
+    prompt: String,
     input: Input,
-    config: Config,
-    context: Context,
+    backend: NumbatWrapper
 }
 
 impl Cli {
+
+
+    fn new(args: Args) -> Result<Self> {
+        let mut config = NumbatConfig::default();
+        config.load_prelude &= !args.no_prelude;
+
+        let input = match args.file {
+            Some(path) => Input::File(path),
+            None => Input::Stdin,
+        };
+
+        let backend = NumbatWrapper::new(config)?;
+
+        Ok(Cli { input,backend, prompt: "#=".to_string() })
+    }
+
+
+    fn run(&mut self) -> Result<()>  {
+
+        let mut batch = vec![];
+        for line in self.input.clone().lines() {
+            if let Some(at) = line.find(&self.prompt) {
+                let (expression, _) = line.split_at(at);
+                batch.push(expression.to_string());
+                print!("{expression}{}", self.prompt);
+
+                let code = batch.join("\n");
+                batch.clear();
+                let value = self.backend.eval_block(code)?;
+                println!(" {value}");
+            } else {
+                println!("{line}");
+                batch.push(line);
+            }
+        }
+
+        Ok(())
+    }
+}
+
+struct NumbatConfig {
+    load_prelude: bool,
+    load_user_init: bool,
+}
+
+impl Default for NumbatConfig{
+    fn default() -> Self {
+        Self {
+            load_prelude: true,
+            load_user_init: true,
+        }
+    }
+}
+
+struct NumbatWrapper {
+    context: Context,
+    config: NumbatConfig,
+}
+
+impl NumbatWrapper {
 
     fn make_fresh_context() -> Context {
         let fs_importer = FileSystemImporter::default();
@@ -83,31 +128,11 @@ impl Cli {
         Context::new(importer)
     }
 
-    fn new(args: Args) -> Result<Self> {
-        let mut config = Config::default();
-        config.load_prelude &= !args.no_prelude;
+    pub fn new(config: NumbatConfig) -> Result<Self> {
+        let mut context = Self::make_fresh_context();
 
-        let input = match args.file {
-            Some(path) => Input::File(path),
-            None => Input::Stdin,
-        };
-        Self::with_config_and_input(config, input)
-    }
-
-    fn with_config_and_input(config: Config, input: Input) -> Result<Self> {
-        let context = Cli::make_fresh_context();
-
-        Ok(Self {
-            input,
-            config,
-            context,
-        })
-    }
-
-    fn run(&mut self) -> Result<()>  {
-
-        if self.config.load_prelude {
-            let result = self.context.interpret(
+        if config.load_prelude {
+            let result = context.interpret(
                 "use prelude",
                 CodeSource::Internal,
             );
@@ -116,32 +141,21 @@ impl Cli {
             }
         }
 
-        let mut batch = vec![];
-        for line in self.input.clone().lines() {
-            if let Some(at) = line.find(&self.config.equals_prompt) {
-                let (expression, _) = line.split_at(at);
-                batch.push(expression.to_string());
-                print!("{expression}{}", self.config.equals_prompt);
+        Ok(Self{ context,config })
+    }
 
-                let code = batch.join("\n");
-                batch.clear();
+    pub fn eval_block(&mut self, block: String) -> Result<CompactString> {
+        let mut block = block;
 
-                let result = self.context.interpret(&code, numbat::resolver::CodeSource::Text);
-                match result {
-                    Err(e) => bail!("Failed {e}"),
-                    Ok((_, res)) => {
-                        let value = res.value_as_string().context("Failed to get value")?;
-                        println!(" {value}")
-                    },
-                }
-            } else {
-                println!("{line}");
-                batch.push(line);
-            }
+        if block.starts_with("let") {
+            let var = block.split_whitespace().nth(1).context("Failed to extract variable")?;
+            block.push_str(format!("\n{var}").as_str());
         }
 
-        Ok(())
+        let (_, res) = self.context.interpret(&block, numbat::resolver::CodeSource::Text)?;
+        res.value_as_string().context("Failed to get value")
     }
+
 }
 
 fn main() {
@@ -152,3 +166,46 @@ fn main() {
         std::process::exit(1);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use numbat::compact_str::ToCompactString;
+    use crate::{NumbatConfig, NumbatWrapper};
+
+    #[test]
+    fn test_minimal_expression() {
+        let mut numbat = NumbatWrapper::new(NumbatConfig::default()).unwrap();
+        let result = numbat.eval_block("1 + 1".to_string());
+        assert_eq!(result.ok(), Some("2".to_compact_string()));
+    }
+
+    #[test]
+    fn test_minimal_expression_with_units() {
+        let mut numbat = NumbatWrapper::new(NumbatConfig::default()).unwrap();
+        let result = numbat.eval_block("1 V + 1 V".to_string());
+        assert_eq!(result.ok(), Some("2 V".to_compact_string()));
+    }
+
+    #[test]
+    fn test_minimal_expression_with_units2() {
+        let mut numbat = NumbatWrapper::new(NumbatConfig::default()).unwrap();
+        let result = numbat.eval_block("1 V + 1 V -> mV".to_string());
+        assert_eq!(result.ok(), Some("2000 mV".to_compact_string()));
+    }
+
+    #[test]
+    fn test_double_line_expression() {
+        let mut numbat = NumbatWrapper::new(NumbatConfig::default()).unwrap();
+        let result = numbat.eval_block("let voltage = 1 V + 1 V -> mV\nvoltage #=".to_string());
+        assert_eq!(result.ok(), Some("2000 mV".to_compact_string()));
+    }
+
+    #[test]
+    fn test_let_expression() {
+        let mut numbat = NumbatWrapper::new(NumbatConfig::default()).unwrap();
+        let result = numbat.eval_block("let voltage = 1 V + 1 V -> mV".to_string());
+        assert_eq!(result.ok(), Some("2000 mV".to_compact_string()));
+    }
+
+}
+
